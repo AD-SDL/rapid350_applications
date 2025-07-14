0c399f7e359082d53f6d0298ec4c9e0a3fea2014
caseystone <cstone@anl.gov>
2025-07-14 16:58:31 -0500

    changes for attempted run

--- demo_AMP/demo_AMP_app.py
+++ demo_AMP/demo_AMP_app.py
@@ -9,32 +9,31 @@ from pathlib import Path
 from helper_functions.hso_functions import package_hso
 from protocols import solo_transfer1
 
-console = Console()
-
-
-class DemoAMPExperimentApplication(ExperimentApplication): 
+class DemoAMPExperimentApplication(ExperimentApplication):
     """Experiment application for the AMP Demo"""
 
     workflow_directory = Path("./workflows").resolve()
     protocol_directory = Path("./protocols").resolve()
+    experiment_design = Path("./experiment_design.yaml")
 
-    # Q: How does it know which workcell to run on? 
+    # Q: How does it know which workcell to run on?
     url = "http://hudson01:8000"  # ???
 
 
-    def run_demo(self): 
+    def run_demo(self):
 
         # workflow path(s)
         demo_wf = self.workflow_directory / "demo_wf.yaml"
 
         # initial payload
         parameters = {
-            "temp": 37.0, #a float value setting the temperature of the Liconic Incubator (in Celsius) 
+            "temp": 37.0, #a float value setting the temperature of the Liconic Incubator (in Celsius)
             "humidity": 95.0, # a float value setting the humidity of the Liconic Incubator
             "shaker_speed": 30, #an integer value setting the shaker speed of the Liconic Incubator
             "stacker": 1, # an integer value specifying which stacker a well plate should be used in (Preferable to use "incubation_plate_id" : plate_id, where plate_id is an integer 1-88 - stacker and slot will be autocalculated)
             "slot": 2, # an integer value specifying which slot a well plate should be used in (Preferable to use "incubation_plate_id" : plate_id, where plate_id is an integer 1-88 - stacker and slot will be autocalculated)
             "tip_box_position": "3", # string of an integer 1-8 that identifies the position of the tip box when it is being refilled
+            "seal_time": 3, # an integer value setting the time in seconds for the sealer to seal a plate
         }
 
         # generate and format SOLO hso protocol, then add to parameters dict
@@ -47,7 +46,7 @@ class DemoAMPExperimentApplication(ExperimentApplication):
 
         # run the demo workflow
         self.workcell_client.submit_workflow(
-            workflow = self.demo_wf, 
+            workflow = demo_wf,
             parameters=parameters
         )
 
