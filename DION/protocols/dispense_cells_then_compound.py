816d95024a26a4c2ccc2e53463e76260b150b62e
Casey Stone <cstone@anl.gov>
2025-08-04 16:06:46 -0500

    more SOLO protocols, not tested

---
+++ DION/protocols/dispense_cells_then_compound.py
@@ -0,0 +1,118 @@
+"""
+Generates SOLO .hso instruction file.
+
+"""
+from liquidhandling import SoloSoft
+from liquidhandling import DeepBlock_96VWR_75870_792_sterile
+
+# TODO: edit z height for deepwell, not flat bottom
+
+# SOLO PROTOCOL STEPS
+def generate_hso_file(
+        payload,
+        temp_file_path,
+):
+    """generate_hso_file
+
+    Description:
+         Dispenses cells from cell stock plate into exposure wells using SOLO liquid handler.
+
+    Args:
+        payload (dict): input variables from the wei workflow (not used in demo)
+        temp_file_path (str): file path to temporarily save hso file to
+    """
+
+# * Other program variables
+    # general SOLO variables
+    flat_bottom_z_shift = 2  # Note: 1 is not high enough (tested)
+
+    """
+    SOLO STEP 1: TRANSFER DMSO INTO DILUTION COLUMN WELLS -----------------------------------------------------------------
+    """
+    # * Initialize soloSoft deck layout
+    soloSoft = SoloSoft(
+        filename=temp_file_path,
+        plateList=[
+            "Empty",
+            "Biorad 384 well (HSP3905)",       # assay plate
+            "DeepBlock.96.VWR-75870-792.sterile",  # dilution plate
+            "DeepBlock.96.VWR-75870-792.sterile",       # stock plate: DMSO, control, and test compounds
+            "TipBox.180uL.Axygen-EVF-180-R-S.bluebox",       # 180uL tip box
+            "DeepBlock.96.VWR-75870-792.sterile",       # cells stock plate
+            "Empty",
+            "Empty",
+        ],
+    )
+
+    # stock cells plate details
+    cells_stock_location = "Position6" # ???
+    cells_stock_column = 1   # ???
+    cells_transfer_volume = 240  # will need to do two transfers
+    half_cells_transfer_volume = cells_transfer_volume / 2
+
+    # exposure/indiacator plate details
+    exposure_indiacator_plate_location = "Position1"
+
+    # compound serial dilutuon plate details
+    dilution_plate_location = "Position3"  # Location of the dilution plate
+    dilution_column = 1  # Column in the dilution plate to dispense DMSO
+    dilution_transfer_volume = 10   # 10uL 
+
+    # mix variables
+    mix_cycles = 5
+    mix_volume = 150
+
+    # ACTIONS
+    # 1. Dispense 240 ul cells into each well of exposure columns 1,2, and 3
+    soloSoft.getTip("Position5")  # 8-channel transfer, same tips for all transfers
+    for i in range(3): # three destination columns (1,2,3)
+        for j in range(2):  # two transfers needed, 120ul each time
+            soloSoft.aspirate(
+                position=cells_stock_location,
+                aspirate_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
+                    cells_stock_column, half_cells_transfer_volume
+                ),
+                aspirate_shift=[0, 0, flat_bottom_z_shift],
+                mix_at_start = True, 
+                mix_cycles = mix_cycles, 
+                mix_volume = mix_volume, 
+                dispense_height = flat_bottom_z_shift
+            )
+            soloSoft.dispense(
+                position=exposure_indiacator_plate_location,
+                dispense_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
+                    (i + 1) , half_cells_transfer_volume
+                ),
+                dispense_shift=[0, 0, flat_bottom_z_shift],
+            )
+    soloSoft.shuckTip()
+
+    # 2. dispense 10ul serial diluted compound into exposure columns 1, 2, and 3
+    for i in range(3):
+        soloSoft.aspirate(
+            position=dilution_plate_location,
+            aspirate_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
+                dilution_column, dilution_transfer_volume
+            ),
+            aspirate_shift=[0, 0, flat_bottom_z_shift],
+            mix_at_start = True, 
+            mix_cycles = mix_cycles, 
+            mix_volume = mix_volume, 
+            dispense_height = flat_bottom_z_shift
+        )
+        soloSoft.dispense(
+            position=exposure_indiacator_plate_location,
+            dispense_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
+                (i + 1), dilution_transfer_volume
+            ),
+            dispense_shift=[0, 0, flat_bottom_z_shift],
+            mix_at_finish = True, 
+            mix_cycles = mix_cycles, 
+            mix_volume = mix_volume,
+            aspirate_height = flat_bottom_z_shift,
+        )
+  
+
+
+
+    soloSoft.savePipeline()
---
+++ DION/protocols/exposure_to_indicator.py
@@ -0,0 +1,81 @@
+"""
+Generates SOLO .hso instruction file.
+
+"""
+from liquidhandling import SoloSoft
+from liquidhandling import DeepBlock_96VWR_75870_792_sterile
+
+# TODO: edit z height for deepwell, not flat bottom
+# TODO: Custom plate def for 48 deepwell 
+# TODO: Clean up and document
+
+# SOLO PROTOCOL STEPS
+def generate_hso_file(
+        payload,
+        temp_file_path,
+):
+    """generate_hso_file
+
+    Description:
+        Dispenses 50ul from one indicator column into each well of a 384 well plate. 
+        Repeats for the other two indicator columns into new 384 well plates. 
+
+    Args:
+        payload (dict): input variables from the wei workflow (not used in demo)
+        temp_file_path (str): file path to temporarily save hso file to
+    """
+
+# * Other program variables
+    # general SOLO variables
+    flat_bottom_z_shift = 2  # Note: 1 is not high enough (tested)
+
+    # * Initialize soloSoft deck layout
+    soloSoft = SoloSoft(
+        filename=temp_file_path,
+        plateList=[
+            "Empty",
+            "Biorad 384 well (HSP3905)",       # assay plate
+            "DeepBlock.96.VWR-75870-792.sterile",  # dilution plate
+            "DeepBlock.96.VWR-75870-792.sterile",       # stock plate: DMSO, control, and test compounds
+            "TipBox.180uL.Axygen-EVF-180-R-S.bluebox",       # 180uL tip box
+            "DeepBlock.96.VWR-75870-792.sterile",       # cells stock plate
+            "Empty",
+            "Empty",
+        ],
+    )
+
+    # exposure/indiacator plate details
+    exposure_indiacator_plate_location = "Position1"
+    exposure_columns = [1,2,3]
+    indicator_columns = [4,5,6]
+    total_transfer_volume = 240 
+    half_total_transfer_volume = total_transfer_volume / 2
+    # NOTE: want to transfer total volume (250uL) but can't with robots
+    # TODO: check that 240 is good, how much could we transfer? <- TEST
+
+    # mix variables
+    mix_cycles = 5
+    mix_volume = 150
+
+    # ACTIONS
+    # 1. Dispense all contents of exposure columns (1,2, and 3) into each well of indicator columns (1,2, and 3)
+    soloSoft.getTip("Position5")  # 8-channel transfer, same tips for all transfers
+    for i in range(3): # three destination columns (1,2,3)
+        for j in range(2):  # two transfers needed, 1ul each time
+            soloSoft.aspirate(
+                position=exposure_indiacator_plate_location,
+                aspirate_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
+                    exposure_columns[i], half_total_transfer_volume
+                ),
+                aspirate_shift=[0, 0, flat_bottom_z_shift],
+            )
+            soloSoft.dispense(
+                position=exposure_indiacator_plate_location,
+                dispense_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
+                    indicator_columns[i], half_total_transfer_volume
+                ),
+                dispense_shift=[0, 0, flat_bottom_z_shift],
+            )
+    soloSoft.shuckTip()
+
+    soloSoft.savePipeline()
